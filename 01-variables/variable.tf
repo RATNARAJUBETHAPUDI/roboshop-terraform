ec242b753ec7df316cabcabd6253701799d15b06
RATNARAJUBETHAPUDI <ratnaesi008@gmail.com>
2023-05-08 14:40:37 +0530

    my third committ for roboshop-terraform!!

---
+++ 01-variables/variable.tf
@@ -0,0 +1,17 @@
+variable "sample_string" {
+  default = "hello world"
+}
+
+## shell scripting equivalent sample_string "hello world"
+
+## To print in shell script we are using echo, following same in terraform variables printing also
+## echo $sample_string
+
+output "saample_string" {
+  value = "var.sample_string"
+}
+## in the above case we are just printing variable , if the varible is a combiantion of strings then we can use
+below syntax
+output "sample_stirng1"{
+        value = "value of sample string = ${var.sample_string}"
+}
